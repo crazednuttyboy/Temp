16d6ba780aebc7e0408146208cccbcdc9b995e95
ShaneH <shanehalewood@hotmail.com>
2024-11-11 18:18:22 +0000

    Add Initial Proj Files

---
+++ Calculator/Maths.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator
+{
+    public class Maths
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+    }
+}
